b0a50d51b94f730cb43b591a25307149312864c8
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2018-11-02 23:41:02 -0400

    fix: Adds a count to prevent unloading
    When components got re-rendered the data would change with it resulting
    in an empty render.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ import PropTypes from "prop-types"
 import _ from "underscore"
 import { isEmpty } from "./utils"
 
+const count = {}
+
+function incrementCount(name) {
+	if (!(name in count)) {
+		count[name] = 0
+	}
+	count[name] = ++count[name]
+	return count[name]
+}
+
+function decrementCount(name) {
+	if (!(name in count)) {
+		count[name] = 0
+	}
+	// Decrement the count by 1
+	count[name] = --count[name]
+	count[name] = Math.max(0, count[name])
+	return count[name]
+}
+
 class DataController extends React.Component {
 	static propTypes = {
 		children: PropTypes.oneOfType([
@@ -16,25 +36,29 @@ class DataController extends React.Component {
 		loadData: PropTypes.func,
 		unloadData: PropTypes.func,
 		renderWithoutData: PropTypes.func,
-		renderWithData: PropTypes.func
+		renderWithData: PropTypes.func,
+		name: PropTypes.string
 	}
 	componentWillUnmount() {
-		const { unloadData } = this.props
-		if (_.isFunction(unloadData)) {
-			unloadData()
-		}
-	}
-	componentDidMount() {
-		const { loadData } = this.props
-		const dataLoaded = this.dataLoaded()
-		if (!dataLoaded) {
-			if (_.isFunction(loadData)) {
-				loadData()
+		const { unloadData, name } = this.props
+		if (name) {
+			const count = decrementCount(name)
+			if (count === 0) {
+				if (_.isFunction(unloadData)) {
+					unloadData()
+				}
+			}
+		} else {
+			if (_.isFunction(unloadData)) {
+				unloadData()
 			}
 		}
 	}
-	componentDidUpdate(prevProps, prevState) {
-		const { loadData } = this.props
+	componentDidMount() {
+		const { loadData, name } = this.props
+		if (name) {
+			const count = incrementCount(name)
+		}
 		const dataLoaded = this.dataLoaded()
 		if (!dataLoaded) {
 			if (_.isFunction(loadData)) {
