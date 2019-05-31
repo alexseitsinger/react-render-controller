c4c7f413d885495edea3bdc8a86f37abfe234f7d
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-31 11:56:55 -0400

    docs: Adds jsdoc.

--- src/index.js
+++ src/index.js
@@ -23,6 +23,57 @@ function decrementCount(name) {
 	return count[name]
 }
 
+/**
+ * @description Controls what to render depending on whether the data is present or not.
+ * @param {Object|Array} data Checked for emptiness.
+ * @param {Function} loadData Invoked to make the data non-empty.
+ * @param {Function} unloadData Invoked to make the data empty.
+ * @param {Function} renderWithoutData Invoked when rendering with empty data.
+ * @param {Function} renderWithData Invoked when rendering with non-empty data.
+ * @param {String} name A name to use to count this data type.
+ * @return {Component}
+ * @example
+ * import React from "react"
+ * import PropTypes from "prop-types"
+ * import { connect } from "react-redux"
+ * import DataController from "@alexseitsinger/data-controller"
+ *
+ * function App({ data, loadData, unloadData }){
+ *   return (
+ *     <DataController
+ *       data={data}
+ *       loadData={loadData}
+ *       unloadData={unloadData}
+ *       renderWithData={() => {
+ *          return data.map((obj, i) => {
+ *            const key = "data" + i.toString()
+ *            return (
+ *              <div key={key}>{obj.name}</div>
+ *            )
+ *          })
+ *       }}
+ *       renderWithoutData={() => <div>No data</div>}
+ *     />
+ *   )
+ * }
+ *
+ * App.propTypes = {
+ *   data: PropTypes.object.isRequired,
+ *   loadData: PropTypes.func.isRequired,
+ *   unloadData: PropTypes.func.isRequired
+ * }
+ *
+ * const mapState = (state) => ({
+ *   data: state.reducer.data
+ * })
+ *
+ * const mapDispatch = (dispatch) => ({
+ *   loadData: () => dispatch(getData()),
+ *   unloadData: () => dispatch(setData({}))
+ * })
+ *
+ * export default connect(mapState, mapDispatch)(App)
+ */
 class DataController extends React.Component {
 	static propTypes = {
 		children: PropTypes.oneOfType([
