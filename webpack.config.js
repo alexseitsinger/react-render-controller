4f1b84441ec5492d175f7ab9f3dbe1419286529e
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-31 11:53:19 -0400

    chore: Adds babel plugins. adds externals.

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,31 @@
 const path = require("path")
+const nodeExternals = require("webpack-node-externals")
 
 module.exports = {
-    entry: "./src/index.js",
-    mode: "production",
-    output: {
-        path: path.resolve("./dist"),
-        filename: "[name].js",
-        libraryTarget: "commonjs2",
-        libraryExport: "default"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                use: "babel-loader",
-            }
-        ]
-    },
+  entry: "./src/index.js",
+  target: "node",
+  mode: "production",
+  output: {
+    path: path.resolve("./dist"),
+    filename: "[name].js",
+    libraryTarget: "commonjs2",
+    libraryExport: "default"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      }
+    ]
+  },
+  externals: [
+    nodeExternals({
+      modulesFromFile: {
+        exclude: ["dependencies"],
+        include: ["peerDependencies", "devDependencies"]
+      }
+    })
+  ]
 }
