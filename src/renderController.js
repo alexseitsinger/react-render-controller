46c6e66a4835668e3a07def973f5036fa776b8b2
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-18 20:04:08 -0400

    feat: Adds pathname skipping for unloading.
    Adds propTypes. Updates JSDoc. Fixes mistyped componentWillUnmount.

--- src/renderController.js
+++ src/renderController.js
@@ -9,14 +9,21 @@ import {
 
 /**
  * @description Controls what to render based on data being empty or not.
- * @param {Object|Array} data Checked for emptiness.
- * @param {Function} load Invoked to make the data non-empty.
- * @param {Function} unload Invoked to make the data empty.
- * @param {Function} renderWithout Invoked when rendering with empty data.
- * @param {Function} renderWith Invoked when rendering with non-empty data.
- * @param {String} name A name to use to count this data type.
- * @param {Number} maxLoads The number of times load can re-try before stopping.
- * @return {Component}
+ * @param {object|array} data Checked for emptiness.
+ * @param {function} [load] Invoked to make the data non-empty.
+ * @param {function} [unload] Invoked to make the data empty.
+ * @param {function} [renderWithout] Invoked when rendering with empty data.
+ * @param {function} [renderWith] Invoked when rendering with non-empty data.
+ * @param {string} [name] A name to use to count this data type.
+ * @param {array} [skipUnloadFor] An array of pathnames to skip invoking unload
+ * for when navigating to them.
+ * @param {string} [currentPathname] The current pathname. Used to determine if
+ * skipUnloadFor test passes.
+ * @param {number} [maxCount=3] The max number of instances before skipping invoking
+ * load.
+ * @param {number} [delay=1000] The number of milliseconds to wait before invoking the
+ * debounced load() and unload().
+ * @return {component}
  * @example
  * import React from "react"
  * import PropTypes from "prop-types"
@@ -26,6 +33,7 @@ import {
  * function App({ data, load, unload }){
  *   return (
  *     <RenderController
+ *       name={"App_data"}
  *       data={data}
  *       load={load}
  *       unload={unload}
@@ -76,14 +84,19 @@ export class RenderController extends PureComponent {
     renderWith: PropTypes.func,
     name: PropTypes.string,
     maxCount: PropTypes.number,
+    skipUnloadFor: PropTypes.arrayOf(PropTypes.string),
+    currentPathname: PropTypes.string,
+    delay: PropTypes.number,
   }
   static defaultProps = {
     maxCount: 3,
+    skipUnloadFor: [],
+    delay: 1000,
   }
   constructor(props) {
     super(props)
-    this.debouncedDoLoad = debounce(this.doLoad)
-    this.debouncedDoUnload = debounce(this.doUnload)
+    this.debouncedDoLoad = debounce(this.doLoad, props.delay)
+    this.debouncedDoUnload = debounce(this.doUnload, props.delay)
   }
   isLoaded = () => {
     const { data } = this.props
@@ -116,11 +129,21 @@ export class RenderController extends PureComponent {
     }
   }
   doUnload = (callback) => {
-    const { unload, name } = this.props
+    const { unload, name, skipUnloadFor, currentPathname } = this.props
+    // Otherwise, if we get an unload function, decrement the count, and unload
+    // if it's at 0. Then, run our callback if we got one.
     if (_.isFunction(unload)) {
       const currentCount = name ? decrementCount(name) : 0
       if (currentCount === 0) {
-        unload()
+        // Skip unloading if the pathname matches a skipped pathname.
+        if (skipUnloadFor && skipUnloadFor.length && currentPathname && currentPathname.length) {
+          if (!(skipUnloadFor.includes(currentPathname))) {
+            unload()
+          }
+        }
+        else {
+          unload()
+        }
         if (_.isFunction(callback)) {
           callback()
         }
@@ -137,13 +160,18 @@ export class RenderController extends PureComponent {
       })
     }
   }
-  componentDidMount() {
-    this.debouncedDoLoad()
-  }
   componentWillUnmount() {
     const { name } = this.props
     this.debouncedDoUnload(() => {
       setAttempted(name, false)
+    })
+  }
+  componentDidMount() {
+    const { name } = this.props
+    this.debouncedDoLoad(true, () => {
+      if (this.isLoaded() === false) {
+        setAttempted(name, true)
+      }
     })
   }
   render() {
