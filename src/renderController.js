9e4b73bccf2c80726879127cd37f028b36fbb342
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-08-31 12:29:03 -0400

    docs: Updates JSDoc.

--- src/renderController.js
+++ src/renderController.js
@@ -15,44 +15,101 @@ import {
 /**
  * Controls what to render based on data being empty or not.
  *
- * @return {component}
+ * @return {function}
  *
  * @param {object} props
  * @param {object|array} props.data
  * Checked for emptiness.
  * @param {function} [props.load]
  * Invoked to make the data non-empty.
- * @param {number} [props.loadDelay=900]
- * The number of seconds before the isLoadAttempted flag is set to True. When
- * this flag is True, the renderFailure() will be invoked to return output
- * instead of renderWithout, if there is empty data.
  * @param {function} [props.unload]
  * Invoked to make the data empty.
- * @param {function} [props.renderWithout]
- * Invoked when rendering with empty data.
  * @param {function} [props.renderWith]
  * Invoked when rendering with non-empty data.
- * @param {function} [props.renderFailure] i
- * nvoked when loading was attempted but failed to produce non-empty data.
- * @param {array} [props.skipUnloadFor]
- * An array of pathnames to skip invoking unload for when navigating to them.
+ * @param {function} [props.renderWithout]
+ * Invoked when rendering with empty data.
  * @param {string} [props.lastPathname]
- * The current pathname. Used to determine if skipUnloadFor test passes.
+ * The pathname of the last page navigated to
+ * @param {string} [props.currentPathname]
+ * The pathname of the current page navigated to.
+ * @param {array} [props.skippedPathnames]
+ * The pathnames to skip unloading for when this component is navigated away
+ * from.
  *
  * @example
+ * // reducer.js
+ * import { combineReducers } from "react-redux"
+ * import { locationsReducer } from "@alexseitsinger/redux-locations"
+ *
+ * import { pageOneReducer } from "pages/page-one/reducer"
+ *
+ * export const createRootReducer = () => combineReducers({
+ *   locations: locationsReducer,
+ *   pages: combineReducers({
+ *     ...
+ *     pageOne: pageOneReducer,
+ *     ...
+ *   }),
+ * })
+ *
+ * // store.js
+ * import {
+ *   createStore as createReduxStore,
+ *   applyMiddleware,
+ *   compose,
+ * } from "redux"
+ * import { locationsMiddleware } from "@alexseitsinger/redux-locations"
+ *
+ * import { createRootReducer } from "reducer"
+ *
+ * export const createStore = (..., initialState, ...) => {
+ *   const rootReducer = createRootReducer()
+ *   const middleware = [
+ *     ...
+ *     locationsMiddleware,
+ *     ...
+ *   ]
+ *   const storeEnhancers = compose(applyMiddleware(...middleware))
+ *   const store = createReduxStore(rootReducer, initialState, storeEnhancers)
+ *   return store
+ * }
+ *
+ * // pages/page-one/index.js
  * import React from "react"
- * import PropTypes from "prop-types"
  * import { connect } from "react-redux"
  * import { RenderController } from "@alexseitsinger/react-render-controller"
  *
- * function App({ data, load, unload }){
+ * import { getPageData, setPageData } from "actions/page-one.js"
+ *
+ * function PageOne({
+ *   states: {
+ *     pageOne: {
+ *       data,
+ *     },
+ *     locations: {
+ *       last,
+ *       current,
+ *     },
+ *   },
+ *   methods: {
+ *     pageOne: {
+ *       loadData,
+ *       unloadData,
+ *     },
+ *   },
+ * }){
  *   return (
  *     <RenderController
  *       data={data}
- *       load={load}
- *       loadDelay={900}
- *       unload={unload}
+ *       load={loadData}
+ *       unload={unloadData}
+ *       lastPathname={last.pathname}
+ *       currentPathname={current.pathname}
+ *       skippedPathnames={[
+ *         "/path/to/page"
+ *       ]}
  *       renderWith={() => {
+ *          // This is rendered when the 'data' object/array is non-empty.
  *          return data.map((obj, i) => {
  *            const key = "data" + i.toString()
  *            return (
@@ -60,27 +117,33 @@ import {
  *            )
  *          })
  *       }}
- *       renderWithout={() => <div>No data</div>}
+ *       renderWithout={() => {
+ *         // This is rendered when the data object/array is empty.
+ *         return (
+ *           <div>No data</div>
+ *         )
+ *       }}
  *     />
  *   )
  * }
  *
- * App.propTypes = {
- *   data: PropTypes.object.isRequired,
- *   load: PropTypes.func.isRequired,
- *   unload: PropTypes.func.isRequired
- * }
- *
- * const mapState = (state) => ({
- *   data: state.reducer.data
+ * const mapState = state => ({
+ *   states: {
+ *     locations: state.locations,
+ *     pageOne: state.pages.pageOne,
+ *   },
  * })
  *
- * const mapDispatch = (dispatch) => ({
- *   load: () => dispatch(getData()),
- *   unload: () => dispatch(setData({}))
+ * const mapDispatch = dispatch => ({
+ *   methods: {
+ *     pageOne: {
+ *       loadData: () => dispatch(getPageData()),
+ *       unloadData: () => dispatch(setPageData({})),
+ *     },
+ *   },
  * })
  *
- * export default connect(mapState, mapDispatch)(App)
+ * export default connect(mapState, mapDispatch)(PageOne)
  */
 export class RenderController extends React.Component {
   static propTypes = {
@@ -140,7 +203,9 @@ export class RenderController extends React.Component {
     if (!(_.isFunction(unload))) {
       return
     }
-    addUnloader(unload, createShouldSkipUnload(lastPathname, currentPathname, skippedPathnames))
+    addUnloader(unload, createShouldSkipUnload(
+      lastPathname, currentPathname, skippedPathnames,
+    ))
   }
 
   isDataEmpty = () => {
