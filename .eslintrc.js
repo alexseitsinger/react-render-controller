7eb134d6d8513b844ee358d6cde13b9deee38359
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-10-21 01:35:50 -0400

    feat: Adds eslint plugins.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,11 @@ const path = require("path")
 
 module.exports = {
   root: true,
+  parser: "babel-eslint",
   settings: {
+    "react": {
+      "version": "detect",
+    },
     "import/resolver": {
       webpack: {
         config: path.resolve("./webpack.config.dev.js"),
