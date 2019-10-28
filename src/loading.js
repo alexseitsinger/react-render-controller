d3cbabbb30569a78d3a832254e8836bacb433eff
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-10-28 16:25:05 -0400

    refactor: Moves delete call.

--- src/loading.js
+++ src/loading.js
@@ -1,27 +1,19 @@
 import _ from "underscore"
 
-import { updateLoadCount } from "./counting"
+import {
+  updateLoadCount,
+} from "./counting"
 import {
   runDelayAmount,
 } from "./delays"
+import {
+  doesLoaderMethodExist,
+  rememberLoaderMethod,
+} from "./methods"
 
 const loaders = {}
 const getTotalLoaders = () => {
   return Object.keys(loaders).length
-}
-
-var loaderMethods = []
-export const resetLoaderMethods = _.debounce(() => {
-  loaderMethods = []
-}, 15000)
-
-export const doesLoaderMethodExist = f => {
-  return loaderMethods.map(fn => fn === f).includes(true)
-}
-export const rememberLoaderMethod = f => {
-  if (doesLoaderMethodExist(f) === false) {
-    loaderMethods.push(f)
-  }
 }
 
 export const addLoader = ({ name, handler, method, callback }) => {
@@ -37,14 +29,13 @@ export const addLoader = ({ name, handler, method, callback }) => {
   rememberLoaderMethod(method)
 
   loaders[name] = () => {
+    delete loaders[name]
     if (isLoadCancelled === true) {
-      delete loaders[name]
       return
     }
     handler()
     updateLoadCount(name)
     callback()
-    delete loaders[name]
   }
 
   return () => {
