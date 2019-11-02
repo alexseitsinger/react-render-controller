dc66ee3ba36aa1d3363e7dfa0891c0044a9853f4
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-11-02 04:30:02 -0400

    chore: Updates eslint config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,21 @@ const path = require("path")
 module.exports = {
   root: true,
   parser: "babel-eslint",
+  env: {
+    "jest/globals": true,
+  },
+  globals: {
+    describe: true,
+    expect: true,
+    it: true,
+    test: true,
+    mount: true,
+    shallow: true,
+    render: true,
+  },
+  plugins: [
+    "jest",
+  ],
   settings: {
     "react": {
       "version": "detect",
@@ -16,5 +31,6 @@ module.exports = {
   extends: [
     "@alexseitsinger/eslint-config-base",
     "@alexseitsinger/eslint-config-react",
+    "plugins:jest/recommended",
   ],
 }
