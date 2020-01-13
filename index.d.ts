d2c6469b92cbe6364cf567fbd48e5f882db5300d
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-01-13 17:01:29 -0500

    chore: Adds types.

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+import React from "react"
+import {
+  Props as RenderControllerProps,
+  State as RenderControllerState,
+} from "./src/RenderController.tsx"
+
+export class RenderController extends React.Component<
+  RenderControllerProps,
+  RenderControllerState
+> {}
