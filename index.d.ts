28dce1e03609efb0ae209028ed3d428179df157c
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-01-16 04:14:47 -0500

    refactor: Updates exported types.

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,46 @@
 import React from "react"
-import {
-  Props as RenderControllerProps,
-  State as RenderControllerState,
-} from "./src/RenderController.tsx"
 
-export class RenderController extends React.Component<
-  RenderControllerProps,
-  RenderControllerState
-> {}
+export interface Pathname {
+  from: string;
+  to: string;
+  reverse?: boolean;
+}
+
+export interface AddUnloaderArgs {
+  lastPathname: string;
+  currentPathname: string;
+  skippedPathnames: Pathname[];
+  handler: () => void;
+  name: string;
+}
+
+export interface LoadTarget {
+  name: string;
+  data: object | [];
+  getter: (...args: any) => void;
+  setter: (o: {} | []) => void;
+  empty: {} | [];
+  excluded?: string[];
+  forced?: boolean;
+  attempted?: boolean;
+  cached?: boolean;
+}
+
+export interface Props {
+  children?: React.ReactNode | React.ReactNode[];
+  targets: LoadTarget[];
+  failDelay?: number;
+  renderFirst?: () => React.ReactElement;
+  renderWith?: () => React.ReactElement;
+  renderWithout?: () => React.ReactElement;
+  lastPathname: string;
+  currentPathname: string;
+  skippedPathnames: Pathname[];
+  name: string;
+}
+
+export interface State {
+  isControllerSeen: boolean;
+}
+
+export class RenderController extends React.Component<Props, State> {}
